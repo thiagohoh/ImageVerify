075ec91eaec88ee3a1616fd7cc1144aab9ac05e5
thiagohoh <thiago14lost@gmail.com>
2015-12-30 06:24:05 -0200

    Create MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,64 @@
+package FisrtCode;
+import java.util.Scanner;
+
+public class Myclass {
+	public static void main(String []args){
+		//WelcomeMessage obj= new WelcomeMessage();
+		//obj.printMessage();
+		//obj.printMessage2();
+		//ArraySomething arr = new ArraySomething();
+		//arr.ArrayInitializer();
+		//ArraySomething arr2 = new ArraySomething();
+		//arr2.MultArrayInitializer();
+		//Whilerino ww = new Whilerino();
+		//int n;
+		//System.out.println();
+		//Getrino gg = new Getrino();
+		//n = gg.GetInt();
+		
+		//ww.Whilemento(n);
+	//ReverserinoNumber rv = new ReverserinoNumber();
+	//rv.RvsN(n);
+	//System.out.println(n);
+	Filerino f = new Filerino();
+	//f.Creatfile();
+	//f.DirectoryCreat();
+	Imagerino img = new Imagerino();
+
+	img.Ibage();
+	int opa =
+			
+	
+	//f.WriterinoSimple();
+	//f.WriterinoSimpleAppend();
+		
+		//GetRGB opa = new GetRGB();
+		//opa.GetImage();
+	
+	//f.ReadorSimple();
+	//f.Renamerino();
+	//f.FileDeleter();
+	//
+	//n2 = gg.GetInt();
+	//System.out.println(n2);
+	 //n2 = rv.RvsN(n2); 
+	//System.out.println(n2);
+	
+//	try {
+		
+	//	obj.printMessage();
+	//}catch(Exception ex)
+	//{
+	//	obj.printMessage2();
+	//}
+//	finally{
+	//	System.out.println("Finnally??");
+	//}
+	
+	
+	}
+	
+	//{
+	
+//}
+}
